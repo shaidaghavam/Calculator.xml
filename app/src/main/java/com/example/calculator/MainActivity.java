c03065c9d895a33c031a3ec0ef8d2cde1d4c5b25
Ariike.ir <shaida.ghavam@gmail.com>
2023-01-14 20:57:22 +0330

    committed

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -1,14 +1,66 @@
 package com.example.calculator;
 
-import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.calculator.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    private Double firstNumber, secondNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+        binding.buttonAdd.setOnClickListener(view -> {
+
+            firstNumber = Double.parseDouble((binding.num1.getText().toString()));
+            secondNumber = Double.parseDouble((binding.num2.getText().toString()));
+
+            binding.result.setText(firstNumber + secondNumber + "");
+
+        });
+        binding.buttonMinimize.setOnClickListener(view -> {
+
+            firstNumber = Double.parseDouble((binding.num1.getText().toString()));
+            secondNumber = Double.parseDouble((binding.num2.getText().toString()));
+
+            binding.result.setText(firstNumber - secondNumber + "");
+
+        });
+
+        binding.buttonDivision.setOnClickListener(view -> {
+
+            firstNumber = Double.parseDouble((binding.num1.getText().toString()));
+            secondNumber = Double.parseDouble((binding.num2.getText().toString()));
+
+            binding.result.setText(firstNumber / secondNumber + "");
+
+        });
+
+
+        binding.buttonMultiple.setOnClickListener(view -> {
+
+            firstNumber = Double.parseDouble((binding.num1.getText().toString()));
+            secondNumber = Double.parseDouble((binding.num2.getText().toString()));
+
+            binding.result.setText(firstNumber * secondNumber + "");
+
+        });
+
     }
 }
+
+
+
+
